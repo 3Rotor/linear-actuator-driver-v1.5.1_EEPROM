2c2089f2d2fff09417541f4b4ca750f232a8efa8
3rotor <michiel@thorax-racing.com>
2018-12-10 15:03:51 +0200

    changes on laptop >> desktop

--- linear-actuator-driver-v1.5.1_EEPROM.ino
+++ linear-actuator-driver-v1.5.1_EEPROM.ino
@@ -19,7 +19,7 @@
 
 SoftwareSerial mySerial(5, 2); // RX, TX
 
-/*  testing git cloud
+/*  testing git cloud  changes on laptop...
    _____        _
   /  ___|      | |
   \ `--.   ___ | |_  _   _  _ __
